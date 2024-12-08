9895b3e2b02f0a14f6df9229b1b4e31473c2f38e
BrunoMreis <brbrno@hotmail.com>
2024-12-08 19:43:40 -0300

    Realiza ajustes de  nomes de variáveis, valores de portas.

--- infra/GrupoSeguranca.tf
+++ infra/GrupoSeguranca.tf
@@ -1,22 +1,21 @@
-resource "aws_security_group" "ssh_cluster" {
-  name        = "ssh_cluster"
+resource "aws_security_group" "sg" {
+  name        = "SG-${var.projectName}"
+  description = "Este grupo e usado na live de terraform"
   vpc_id      = module.vpc.vpc_id
-}
 
-resource "aws_security_group_rule" "ssh_cluster_in" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
-  security_group_id = aws_security_group.ssh_cluster.id
-}
+  ingress {
+    description = "All"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-resource "aws_security_group_rule" "ssh_cluster_out" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
-  security_group_id = aws_security_group.ssh_cluster.id
+  egress {
+    description = "All"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
--- infra/Variaveis.tf
+++ infra/Variaveis.tf
@@ -1,3 +1,26 @@
-variable "cluster_name" {
-  type = string
+variable "regionDefault" {
+  default = "us-east-1"
+}
+
+variable "projectName" {
+  default = "nginx"
+}
+
+variable "vpcCidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "instanceType" {
+  default = "t2.micro"
+}
+
+variable "accountIdVoclabs" {}
+
+
+variable "policyArn" {
+  default = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+}
+
+variable "accessConfig" {
+  default = "API_AND_CONFIG_MAP"
 }
