3a383df0887882be80f5abb44760de55b195af58
BrunoMreis <brbrno@hotmail.com>
2024-12-08 19:43:40 -0300

    Remove modulo EKS que não tinha permicao no awsacademy

--- env/prod/Main.tf
+++ env/prod/Main.tf
@@ -2,6 +2,8 @@ module "prod" {
   source = "../../infra"
 
   cluster_name     = "producao"
+  accountIdVoclabs = "992382801838"
+
 }
 
 
@@ -12,3 +14,4 @@ output "easyorder_cluster_endpoint" {
 output "easyorder_cluster_certificate_authority" {
   value = module.prod.eks_cluster_certificate_authority
 }
+ 
---
+++ infra/EKS-cluster.tf
@@ -0,0 +1,14 @@
+resource "aws_eks_cluster" "eks-cluster" {
+  name     = var.projectName
+  role_arn = data.aws_iam_role.labrole.arn
+
+  vpc_config {
+    #subnet_ids         = [for subnet in data.aws_subnet.subnet : subnet.id if subnet.availability_zone != "${var.regionDefault}e"]
+    security_group_ids = [aws_security_group.sg.id]
+    subnet_ids = module.vpc.private_subnets
+  }
+
+  access_config {
+    authentication_mode = var.accessConfig
+  }
+}
