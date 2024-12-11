4f85a32d3000f17afee2a63dd2e54995e5da7bab
BrunoMreis <brbrno@hotmail.com>
2024-12-10 22:12:09 -0300

    Renomea pasta e move

---
+++ src/terraform/resources/eks/SaidaInfo.tf
@@ -0,0 +1,12 @@
+
+output "easyorder_cluster_endpoint" {
+  value = module.prod.eks_cluster_endpoint
+}
+
+output "easyorder_cluster_certificate_authority" {
+  value = module.prod.eks_cluster_certificate_authority
+}
+
+output "easyorder_cluster_name" {
+  value = module.prod.eks_cluster_name
+}
