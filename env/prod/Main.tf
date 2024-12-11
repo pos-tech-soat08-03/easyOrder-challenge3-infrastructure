4fdf15cd7db2a9bc99675aefb74c50718fe2b731
BrunoMreis <brbrno@hotmail.com>
2024-12-10 21:37:43 -0300

    Reconfigura vairaveis

--- env/prod/Main.tf
+++ env/prod/Main.tf
@@ -1,20 +1,13 @@
 module "prod" {
   source = "../../infra"
 
-  
-}
+
+  accountIdVoclabs = var.accountIdVoclabs
+
+ 
 
 
-output "easyorder_cluster_endpoint" {
-  value = module.prod.eks_cluster_endpoint
 }
 
-output "easyorder_cluster_certificate_authority" {
-  value = module.prod.eks_cluster_certificate_authority
-}
-
-output "easyorder_cluster_name" {
-  value = module.prod.eks_cluster_name
-}
 
  
