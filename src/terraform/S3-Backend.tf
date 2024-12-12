d3963ab9ddb141132cc60feb2e1cbe654c29e599
Marcio Saragiotto <marcio.saragiotto@gmail.com>
2024-12-12 12:04:18 -0300

    fix: ajuste bucket backend

--- src/terraform/S3-Backend.tf
+++ src/terraform/S3-Backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = var.backendBucketVoclabs
+    bucket = "terraform-state-easyorder-5457a05cc9784e29b347c29f82dc19cc"
     key    = "easyorder-infra/terraform.tfstate"
     region = "us-east-1"
   }
