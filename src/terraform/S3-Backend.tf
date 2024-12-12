8ba3e227c1d89561e3041bac341d82c7a12a1fec
Marcio Saragiotto <marcio.saragiotto@gmail.com>
2024-12-12 11:21:11 -0300

    chore: nomenclatura bucket para teste CI

--- src/terraform/S3-Backend.tf
+++ src/terraform/S3-Backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-easyorder2"
+    bucket = "terraform-state-easyorder"
     key    = "easyorder-infra/terraform.tfstate"
     region = "us-east-1"
   }
