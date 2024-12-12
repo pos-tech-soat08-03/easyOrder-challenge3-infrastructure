31f603af4738fc22f021c11c507f64990942eaeb
Marcio Saragiotto <marcio.saragiotto@gmail.com>
2024-12-12 15:09:00 -0300

    fix: ajuste formato

--- src/terraform/S3-Backend.tf
+++ src/terraform/S3-Backend.tf
@@ -1,16 +1,3 @@
-variable "bucket" {
-  description = "The S3 bucket to store the Terraform state file"
-  default = "terraform-state-easyorder"
-}
-variable "key" {
-  description = "The S3 key to store the Terraform state file"
-  default = "easyorder-infra/terraform.tfstate"
-}
-variable "region" {
-  description = "The S3 region to store the Terraform state file"
-  default = "us-east-1"
-}
-
 terraform {
   backend "s3" {
     bucket = "${var.bucket}"
--- src/terraform/Variables.tf
+++ src/terraform/Variables.tf
@@ -24,3 +24,16 @@ variable "policyArn" {
 variable "regionDefault" {
   default = "us-east-1"
 }
+
+variable "bucket" {
+  description = "The S3 bucket to store the Terraform state file"
+  default = "terraform-state-easyorder"
+}
+variable "key" {
+  description = "The S3 key to store the Terraform state file"
+  default = "easyorder-infra/terraform.tfstate"
+}
+variable "region" {
+  description = "The S3 region to store the Terraform state file"
+  default = "us-east-1"
+}
