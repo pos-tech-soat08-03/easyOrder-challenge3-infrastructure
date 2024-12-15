188aed707f979f059fa0148dcdf6061284ab1af4
BrunoMreis <brbrno@hotmail.com>
2024-12-15 20:13:07 -0300

    corrige saidas para usar no rds

--- src/terraform/Outputs.tf
+++ src/terraform/Outputs.tf
@@ -11,14 +11,13 @@ output "easyorder_cluster_name" {
   value = aws_eks_cluster.eks-cluster.name
 }
 
-output "vpc_id" {
-  value = aws_eks_cluster.eks-cluster.vpc_config[*].vpc_id
-}
-
-output "subnet_ids" {
-  value = aws_eks_cluster.eks-cluster.vpc_config[*].subnet_ids[*]
-}
-
 output "security_group_id" {
-  value = aws_eks_cluster.eks-cluster.vpc_config[*].security_group_ids[*]
+  value = aws_security_group.gruposeguranca.id
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnets
+}
+output "vpc_id" {
+  value = module.vpc.vpc_id
 }
